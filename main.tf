2894d597487ef08a1a20af704603db3edf2c1670
Гринько Василий Владимирович <vvgrink1@mts.ru>
2021-11-13 12:57:56 +0300

    complete tasks

--- main.tf
+++ main.tf
@@ -1,10 +1,93 @@
 provider "aws" {
     region = "us-east-2"
 }
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    tags = {
-        Name = "TF-example"
+    name = "terraform-example-instance"
+    security_groups = [aws_security_group.instance.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello world" > index.html
+                    nohup busybox httpd -f -p 8080 & 
+                EOF
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+    data "aws_vpc" "default" {
+        default = true
+    }
+    data "aws_subnet_ids" "default" {
+        vpc_id = data.aws_vpc.default.id
+    }
+resource "aws_autoscaling_group" "example" {
+
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+    min_size = 2
+    max_size = 3
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lib_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+
+
+resource "aws_security_group" "alb" {
+    name = "tarraform-example-alb"
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_treshold = 2
+        unhealthy_treshold = 2
     }
 }
